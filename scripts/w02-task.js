5981eedcdbce7696e6a194f674a705e7bae99f76
evapearl <122605689+evapearl@users.noreply.github.com>
2023-09-23 21:16:56 +0100

    week 2 programming task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,16 +3,18 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+// Declare and instantiate variables for name, year and image 
 let fullName = 'Eunice Ebevuhe';
-const currentYear = date();
-let profilePicture = 'images/img.jpg';
+const currentYear = Date();
+const profilePicture = 'images/img.jpg';
+console.log(`It is now ${currentYear}`);
 
 
 
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('favFood');
+const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('img');
 
@@ -23,23 +25,45 @@ const imageElement = document.querySelector('img');
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = `${currentYear}`;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', `Profile image of ${profilePicture}`);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 
 
 
 /* Step 5 - Array */
-let favFood = ['Jellof rice', 'Sauce gombo','Pasta', 'Moi moi'];
-let fav = 'Coconut rice';
-favFood.push('Coconut rice');
-foodElement.innerHTML += `<br>${favFood}`;
-favFood.shift();
-foodElement.innerHTML = `<br>${favFood}`;
-favFood.pop();
-foodElement.innerHTML = `<br>${favFood}`;
+// array of favourite food
+let favFoods = ['Jellof rice', 'Pancake','Pasta', 'Moi Moi'];
 
+// loop and display original array
+for (let i = 0; i < favFoods.length; i++) {foodElement.innerHTML += `${favFoods[i]}`;
+}
+foodElement.innerHTML += '<br>';
 
+// add new favourite item
+favFoods.push(",chips");
 
+// Display updated array
+for (let i = 0; i < favFoods.length; i++){foodElement.innerHTML += `${favFoods[i]}`;
+}
+foodElement.innerHTML += '<br>';
 
+// remove first item
+favFoods.shift();
 
+// display array with item removed
+for (let i = 0; i < favFoods.length; i++){foodElement.innerHTML += `${favFoods[i]}`;
+}
+foodElement.innerHTML += '<br>';
+
+//remove last item
+favFoods.pop();
+
+// display final array
+for (let i = 0; i < favFoods.length; i++){foodElement.innerHTML += `${favFoods[i]}`;
+}
+foodElement.innerHTML += '<br>';
+
+foodElement.innerHTML = `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML = `<br>${favFoods}`;
