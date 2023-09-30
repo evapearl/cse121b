42d101c71464695c2b145545b2a24e6d9ae5b496
evapearl <122605689+evapearl@users.noreply.github.com>
2023-09-30 08:38:30 +0100

    made some change to the program

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -6,77 +6,58 @@ function add (number1, number2) {
     return number1 + number2;
 }
 function addNumbers () {
-    let addNumber1 =Number(document.getElementById('#add1').value);
-    let addNumber2 = Number(document.getElementById('#add2').value);
-    let addNumbers = parseInt(addNumber1 + addNumber2);
-    console.log(addNumbers);
-
-    document.querySelector('#sum').value = add(addNumber1, addNumber2);
-    document.querySelector('#sum').innerHTML = value(add); 
-
-    document.querySelector('#addNumbers').addEventListener('click', addNumbers);
-
+    let add1 =Number(document.getElementById('add1').value);
+    let add2 = Number(document.querySelector('#add2').value);
+    document.getElementById('sum').innerHTML = add(add1, add2);
 }
+
+document.getElementById('addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
 const subtract = function(number1, number2) {
     return `${number1} - ${number2}`;
 }
 function subtractNumbers () {
-    const subtractNumber1 = Number(document.getElementById('#subtract1').value);
+    const subtractNumber1 = Number(document.getElementById('subtract1').value);
     const subtractNumber2 = Number(document.getElementById('#subtract2').value);
-    const subtractNumbers = parseInt(subtractNumber1 - subtractNumber2);
-    console.log(subtractNumbers);
-
-    document.querySelector('difference').value = subtract;
-    document.querySelector('difference').innerHTML = value(subtract);
-
-    document.querySelector(subtractNumbers).addEventListener('click', subtractNumbers);
+    document.getElementById("difference").innerHTML = subtract(subtractNumber1, subtractNumber2);
 }
+
+document.querySelector('subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
 const Multiply = (number1, number2) => `${number1} * ${number2}`;
 
 function multiplyNumbers () {
-    const multiplyNumber1 = Number(document.getElementById('#factor1').value);
+    const multiplyNumber1 = Number(document.getElementById('factor1').value);
     const multiplyNumber2 = Number(document.getElementById('#factor2').value);
-    const multiplyNumbers = parseInt(multiplyNumber1 - multiplyNumber2);
-    console.log(multiplyNumbers);
-
-    document.querySelector('product').value = subtract;
-    document.querySelector('product').innerHTML = value(subtract);
-
-    document.querySelector(multiplyNumbers).addEventListener('click', multiplyNumbers);
+    document.getElementById("product").innerHTML = Multiply(multiplyNumber1, multiplyNumber2);
 }
+document.querySelector('multiplyNumbers').addEventListener('click', multiplyNumbers);
+
 
 /* Open Function Use - Divide Numbers */
 function divid (dividend, divisor) {
     return dividend + divisor;
 }
 function dividNumbers () {
-    let dividend =Number(document.getElementById('#dividend').value);
+    let dividend =Number(document.getElementById('dividend').value);
     let divisor = Number(document.getElementById('#divisor').value);
-    let dividNumbers = parseInt(dividend + divisor);
-    console.log(dividNumbers);
-
-    document.querySelector('#quotient').value = divid(dividend, divisor);
-    document.querySelector('#quotient').innerHTML = value(divid);  
-
-    document.querySelector('#dividNumbers').addEventListener('click', dividNumbers);
-
+    document.getElementById("quotient").innerHTML= divid(dividend + divisor);
 }
+document.querySelector('dividNumbers').addEventListener('click', dividNumbers);
+
 
 /* Decision Structure */
+let today =new Date();
+console.log(today);
 
-const currentDate = Date();
-//console.log(`It is now ${currentDate}`);
-
-const currentYear = currentYear;
+let currentYear = '';
 //console.log(`${currentYear}`);
 
-currentDate.getFullYear= currentYear;
+currentYear= today.getFullYear();
 
-document.getElementById('#year').innerHTML = currentYear;
+document.getElementById('year').textContent = currentYear;
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
